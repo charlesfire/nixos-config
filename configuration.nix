39b9e244688d163a8404102753549800ac99cb39
Charles Lachance <charleslachance@hotmail.com>
2025-04-27 20:55:59 -0400

    Added rootless docker

--- configuration.nix
+++ configuration.nix
@@ -126,6 +126,14 @@
     })
   ];
 
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
